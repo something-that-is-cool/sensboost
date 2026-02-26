c23d49edd4cd0289c5de316e7571cb57b53053da
k4ties <ezzelgavnezzel@gmail.com>
2026-02-26 16:36:17 +0300

    remove icon 2

--- build.bat
+++ build.bat
@@ -4,6 +4,6 @@ for /f "tokens=*" %%i in ('git rev-parse --short HEAD') do set GIT_COMMIT=%%i
 set PKG=github.com/something-that-is-cool/zutil/internal/version
 
 if not exist app.syso (
-    rsrc -manifest app.exe.manifest -o app.syso -ico icon.ico
+    rsrc -manifest app.exe.manifest -o app.syso
 )
 go build -ldflags="-s -w -H=windowsgui -X '%PKG%.Version=%GIT_VERSION%' -X '%PKG%.Commit=%GIT_COMMIT%'" -tags no_emoji -o zutil.exe
